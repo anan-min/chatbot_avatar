22d6a0842903b8675a38216d56ee350f632471fe
nut <nut999anan@hotmail.com>
2025-01-27 17:58:53 +0700

    cube avatar

--- src/components/home/Canva.jsx
+++ src/components/home/Canva.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
+import { Experience } from "@/components/avatar/Experience";
 
 export const Canva = () => {
   return (
     <Canvas shadows camera={{ position: [0, 0, 8], fov: 42 }}>
-      <color attach="background" args={["#ececec"]} />
+      <color attach="background" args={["#ffffff"]} />
+      <Experience />
     </Canvas>
   );
 };
