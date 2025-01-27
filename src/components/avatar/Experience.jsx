59c70cacfe1a5eb2579b482b53661f00272e6796
nut <nut999anan@hotmail.com>
2025-01-27 18:52:54 +0700

    avatar working

--- src/components/avatar/Experience.jsx
+++ src/components/avatar/Experience.jsx
@@ -1,12 +1,18 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Environment, useTexture } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { Avatar } from "@/components/avatar/Avatar";
 
 export const Experience = () => {
+  const texture = useTexture("/textures/background2.jpg");
+  const viewPort = useThree((state) => state.viewport);
+
   return (
     <>
-      <OrbitControls />
+      <Avatar position={[0, -5, 5]} scale={3} />
+      <Environment preset="sunset" />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <planeGeometry args={[viewPort.width, viewPort.height]} />
+        <meshBasicMaterial map={texture} />
       </mesh>
     </>
   );
